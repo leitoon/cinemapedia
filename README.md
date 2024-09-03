07f0c83869f45392be04fdbdb2d2ac7afe7f7b69
krikamobile <krikamobile@gmail.com>
2024-09-03 09:39:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cinemapedia
+
+# Dev
+1. Copiar el .env.template y renombralo a .env
+2. Cambiar las variables de entorno 
---
+++ lib/domain/datasources/movies_datasources.dart
@@ -0,0 +1,8 @@
+
+
+
+import '../entities/movie.dart';
+
+abstract class MovieDatasource {
+  Future<List<Movie>> getNowPlaying ({int page = 1});
+}
