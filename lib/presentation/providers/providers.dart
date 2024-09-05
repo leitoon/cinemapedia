d107982aef0990539ff1ae025bbf908a80e13daf
krikamobile <krikamobile@gmail.com>
2024-09-05 10:26:03 -0500

    ver las peliculas

---
+++ lib/presentation/providers/providers.dart
@@ -0,0 +1,5 @@
+
+
+export 'package:cinemapedia/presentation/providers/movies/movies_provider.dart';
+export 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cinemapedia/presentation/providers/movies/movies_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -8,8 +10,40 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Placeholder(),
+        child: _HomeView(),
       ),
     );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView({
+    super.key,
+  });
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index)  {
+        final movie= nowPlayingMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      }
+      
+      );
   }
 }
